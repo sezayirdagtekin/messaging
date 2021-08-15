be9d3b228903b8d35b7009f58ff0fda2cab0bf09
Sezayir Dağtekin <sezayir.dagtekin@hurriyet.do.net.tr>
2021-08-15 16:15:23 +0300

    refactor

--- rabbitmq/demo/src/main/java/com/sezayir/demo/config/RabbitMQConfig.java
+++ rabbitmq/demo/src/main/java/com/sezayir/demo/config/RabbitMQConfig.java
@@ -8,11 +8,8 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 @Configuration
 public class RabbitMQConfig {
